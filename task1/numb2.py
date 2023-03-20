978eac389134998f3748dfd1e338d40466005cee
Nopalogic <naufaladhi02@gmail.com>
2023-03-20 20:49:04 +0700

    first commit

---
+++ task1/numb2.py
@@ -0,0 +1,9 @@
+nama = input("Masukkan nama: ")
+nim = int(input("Masukkan NIM: "))
+umur = int(input("Masukkan umur: "))
+motivasi = input("Masukkan motivasi anda: ")
+
+print("Nama:", nama)
+print("NIM:", nim)
+print("Umur:", umur)
+print("Motivasi:", motivasi)
---
+++ task1/numb3.py
@@ -0,0 +1,8 @@
+a =31
+b=7
+c=29
+d=12
+
+result = (a+b)//c*d
+
+print("Hasil dari (", a,'+',b,') //',c,'*',d,'adalah', result)
