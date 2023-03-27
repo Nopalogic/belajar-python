fe99b0a765e8b19cd57055656ceae2566a964761
Nopalogic <naufaladhi02@gmail.com>
2023-03-27 12:13:23 +0700

    update

--- task1/numb3.py
+++ task1/numb3.py
@@ -3,6 +3,23 @@ b=7
 c=29
 d=12
 
-result = (a+b)//c*d
+# result = (a+b)//c*d
 
-print("Hasil dari (", a,'+',b,') //',c,'*',d,'adalah', result)
+# print("Hasil dari (", a,'+',b,') //',c,'*',d,'adalah', result)
+
+
+penjumlahan = a + b
+pengurangan = c -  d
+perkalian = b * 2
+pembagian = b / 2
+modulus = a % 2
+exponen = b ** 2
+floorDiv = b // 2
+
+print("Hasil penjumlahan:",penjumlahan)
+print("Hasil pengurangan:",pengurangan)
+print("Hasil perkalian:",perkalian)
+print("Hasil pembagian:",pembagian)
+print("Hasil modulus:",modulus)
+print("Hasil exponen:",exponen)
+print("Hasil floor division:",floorDiv)
