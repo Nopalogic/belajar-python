c872cbad3595d20973f3fff08e0fc476ce04a751
Nopalogic <naufaladhi02@gmail.com>
2023-03-27 12:13:14 +0700

    first commit

---
+++ task2/quiz.py
@@ -0,0 +1,13 @@
+# Keliling persegi panjang
+# panjang = int(input("masukkan nilai panjang: "))
+# lebar = int(input("masukkan nilai lebar: "))
+
+# keliling = 2*(panjang + lebar)
+
+# print("Keliling persegi panjang adalah", keliling)
+
+# Volume kubus
+sisi = int(input("masukkan nilai sisi: "))
+volume = sisi ** 3
+
+print("Volume kubus adalah", volume)
