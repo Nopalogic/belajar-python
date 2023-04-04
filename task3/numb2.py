3a7c8d657f9f7af9985f151c8e221782a3a81e9b
Nopalogic <naufaladhi02@gmail.com>
2023-04-04 07:54:42 +0700

    first commit

---
+++ task3/numb2.py
@@ -0,0 +1,14 @@
+print("""
+1. Keliling
+2. Luas
+""")
+
+choose = int(input("Masukkan angka: "));
+
+if choose == 1:
+    print("Trapesium")
+elif choose == 2:
+    print("Lingkaran")
+else:
+    print("Yang bener anjir")
+    
